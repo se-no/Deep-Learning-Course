3c95ca61ebd94288da530182fad245049dfff956
se-no <61681829+se-no@users.noreply.github.com>
2020-03-06 16:44:08 +0800

    第一次作业
    一元二次方程：输入a,b,c的值，输出x的解。

--- se-no'sFristHomework.py
+++ se-no'sFristHomework.py
@@ -1,15 +1,15 @@
 import os
 import math
-def strtodigit(s):						#传入变量的名字
-	x = input("请输入 "+ s +" 的值：")	#“-123.45”
-	flag = False						#判定字符串里面有没有 “-”，是不是负数
+def strtodigit(s):						
+	x = input("请输入 "+ s +" 的值：")	
+	flag = False						
 	if x[0:1]=='-':
-		flag = True						#True
-		x=x[1:]							#“123.45”
-	while not x.isdigit():				#not x(不)是数字(“-123.45”)的话
+		flag = True						
+		x=x[1:]							
+	while not x.isdigit():				
 		x = input("无效的数据，请重新输入 "+ s +" 的值：")
 	if flag:
-		return -float(x);				#-123.45	
+		return -float(x);				
 	else:
 		return float(x);
 
@@ -23,7 +23,7 @@ if a==0:
 			print("x ∈ (-∞，+∞)")
 		else:#0=0x2+0x+!0 => 0 = !0(1,2,3,4)
 			print("x 无解！")
-	else:#0=0x2 + bx +c	->  bx+c=0  x=-c/b
+	else:
 		print("x=%.6f"%(-c/b))
 else:# a!=0 一元二次
 	d = b*b-4*a*c
